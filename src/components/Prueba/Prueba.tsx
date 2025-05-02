2441b5c7c7afc70c2823c78c3f082c4b9be1b28e
Javichu00 <fercho7649@gmail.com>
2025-05-02 09:16:55 +0200

    Board Drawing

--- src/components/Prueba/Prueba.tsx
+++ src/components/Prueba/Prueba.tsx
@@ -1,41 +1,31 @@
 import { useState } from 'react'
-import reactLogo from '../../assets/react.svg'
-import viteLogo from '/vite.svg'
 import './Prueba.css'
 
 function Prueba() {
-  const [count, setCount] = useState(0)
-  const prueba = [1, 2, 3, 2, 5]
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="table-container">
+        <table>
+          <tbody>
+              <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+              </tr>
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <ul>
-          {
-            prueba.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))
-          }
-        </ul>
-        <p>
-          Edit <code>src/Prueba.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
