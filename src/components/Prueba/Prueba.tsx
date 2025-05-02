7c1a8af3d629056da90561960f8e8e25c463f39b
Javichu00 <fercho7649@gmail.com>
2025-05-02 10:18:09 +0200

    Adecuately Checking Winner

--- src/components/Prueba/Prueba.tsx
+++ src/components/Prueba/Prueba.tsx
@@ -1,9 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Prueba.css';
 
 function Prueba() {
   const [board, setBoard] = useState(Array(9).fill(''));
   const [prevMove, setPrevMove] = useState('O');
+
+  useEffect(() => {
+    checkWinner();
+  }, [board]); // Ejecuta checkWinner cada vez que el tablero cambia
+
+  function checkWinner() {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setTimeout(() => alert(`Ganador: ${board[a]}`), 0); // Asegura que el estado se actualice antes de alertar
+        setBoard(Array(9).fill('')); // Reinicia el tablero
+        setPrevMove("O")
+        return;
+      }
+    }
+
+  }
 
   function drawMove(cellId: number) {
     if (board[cellId] === '') {
