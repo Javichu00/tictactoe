49f61b062ef2c336d81d82d29e494996d4fff9f4
Javichu00 <fercho7649@gmail.com>
2025-05-02 12:33:00 +0200

    Add Header component with styling for game title and instructions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import TTTLogic from './components/TTTLogic/TTTLogic'
+import Header from './components/aesthetics/Header.tsx'
 
 function App() {
 
   return (
     <>
+      <Header />
       <TTTLogic />
     </>
   )
---
+++ src/components/aesthetics/Header.tsx
@@ -0,0 +1,16 @@
+import './Header.css';
+
+
+
+function Header() {
+  return (
+    <>
+        <div className="header">
+            <h1>Tic Tac Toe</h1>
+            <p>Press 1-9 or q-w-e-a-s-d-z-x-c or clic on the cells to play</p>
+        </div>
+    </>
+  );
+}
+
+export default Header;
