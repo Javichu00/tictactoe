50e85f285ad11be0e97169434b644997f849a7df
Javichu00 <fercho7649@gmail.com>
2025-05-02 09:59:22 +0200

    Working drawMove

--- src/components/Prueba/Prueba.tsx
+++ src/components/Prueba/Prueba.tsx
@@ -1,28 +1,41 @@
-import { useState } from 'react'
-import './Prueba.css'
+import { useState } from 'react';
+import './Prueba.css';
 
 function Prueba() {
+  const [board, setBoard] = useState(Array(9).fill(''));
+  const [prevMove, setPrevMove] = useState('O');
 
-
+  function drawMove(cellId: number) {
+    if (board[cellId] === '') {
+      const newBoard = [...board];
+      newBoard[cellId] = prevMove === 'X' ? 'O' : 'X';
+      setBoard(newBoard);
+      setPrevMove(prevMove === 'X' ? 'O' : 'X');
+    } else {
+      alert('Celda ocupada');
+    }
+  }
 
   return (
-    <>
-      <div className="table-container">
-        <table>
-          <tbody>
-            {Array.from({ length: 3 }, (_, i) => (
-              <tr key={i}>
-                {Array.from({ length: 3 }, (_, j) => (
-                  {const cellId = i*3+j+1}
-                  <td key={j} onClick={function1()}>Cell </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  )
+    <div className="table-container">
+      <table>
+        <tbody>
+          {Array.from({ length: 3 }, (_, i) => (
+            <tr key={i}>
+              {Array.from({ length: 3 }, (_, j) => {
+                const cellId = i * 3 + j;
+                return (
+                  <td key={cellId} onClick={() => drawMove(cellId)}>
+                    {board[cellId]}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Prueba
+export default Prueba;
